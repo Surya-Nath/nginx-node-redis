347c0462f0506d06d903408961415d5ac92106f5
Ubuntu <ubuntu@ip-172-31-8-190.ap-south-1.compute.internal>
2026-09-07 06:25:09 +0000

    compose.prod, terraform ec2, ansible deploy

---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "aws_region" { default = "ap-south-1" }
+variable "key_name"   { default = "week1-ansible" }
+variable "ssh_cidr"   { default = "13.233.117.22/32" }
+variable "instance_type" { default = "t3.small" }
